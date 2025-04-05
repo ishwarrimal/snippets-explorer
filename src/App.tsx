0e0cdcc28774e9aa1d72a58d436a14e941db19fb
ishwar rimal <ishwarrimal@ishwars-MacBook-Pro.local>
2025-04-05 12:26:50 +0530

    feat: github pages deployment workflows

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/snippets-explorer/">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
